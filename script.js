d15e5260643699d01463d543df09c2c361f06741
Oleh <oleg_bob@ukr.net>
2022-05-17 20:56:18 +0300

    did some js tasks

--- script.js
+++ script.js
@@ -1,20 +1,84 @@
 "use strict";
 
-let numberOfFilms = +prompt("How many movies you allready watched", 0);
-let lastMovie = prompt("One of the last movies you saw?");
-let rate = prompt("How much would you rate it?");
-let lastMovie2 = prompt("One of the last movies you saw?");
-let rate2 = prompt("How much would you rate it?");
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
+  start: function () {
+    this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    while (this.count == "" || this.count == null || isNaN(this.count)) {
+      this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+  },
+  remenberMyFilm: function () {
+    for (let i = 0; i < 2; i++) {
+      const lastMovie = prompt("Один из последних просмотренных фильмов?", ""),
+        rate = prompt("На сколько оцените его?", "");
+
+      if (
+        lastMovie != null &&
+        rate != null &&
+        lastMovie != "" &&
+        rate != "" &&
+        lastMovie.length < 50
+      ) {
+        this.movies[lastMovie] = rate;
+        console.log("done");
+      } else {
+        console.log("error");
+        i--;
+      }
+    }
+  },
+  detectPersonallevel: function () {
+    if (this.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (this.count >= 10 && this.count < 30) {
+      console.log("Вы классический зритель");
+    } else if (this.count >= 30) {
+      console.log("Вы киноман");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(this);
+    }
+  },
+  wrightYourGenres: function () {
+    for (let i = 0; i < 3; i++) {
+      let answer = prompt(`Your favorite genre number ${i + 1}?`);
+      if (answer === null || answer === "") {
+        answer = prompt(`Your favorite genre number ${i + 1}?`);
+        i--;
+      } else {
+        this.genres[i] = answer;
+      }
+    }
+    this.genres.forEach((element, i) => {
+      console.log(`Любимый жанр #${i + 1} - это ${element}`);
+    });
+  },
+  toggleVisibleMyDB: function () {
+    this.privat ? (this.privat = false) : (this.privat = true);
+  },
 };
 
-personalMovieDB.movies[lastMovie] = rate;
-personalMovieDB.movies[lastMovie2] = rate2;
+// personalMovieDB.remenberMyFilm();
+// personalMovieDB.detectPersonallevel();
+// personalMovieDB.wrightYourGenres();
+// personalMovieDB.showMyDB(personalMovieDB.privat);
+// personalMovieDB.toggleVisibleMyDB();
 
-console.log(personalMovieDB);
+function sum() {
+  let i = 0;
+  i++;
+  console.log(i);
+  while (i < 10) {}
+  sum();
+}
+
+sum();
