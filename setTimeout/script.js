9e7194ca3993b441122fe05948f670d7241c5f8b
Oleh <oleg_bob@ukr.net>
2022-05-23 22:15:20 +0300

    done

---
+++ setTimeout/script.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector(".btn");
+
+// let timerID,
+//   i = 0;
+
+function myAnimation() {
+  const elem = document.querySelector(".box");
+  let pos = 0;
+
+  const id = setInterval(frame, 50);
+
+  function frame() {
+    if (pos == 300) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+      elem.style.left = pos + "px";
+    }
+  }
+}
+
+btn.addEventListener("click", myAnimation);
+
+// clearInterval(timerID);
+
+// function logger(params) {
+//   i === 3 && clearInterval(timerID);
+//   console.log("text");
+//   i++;
+// }
+
+// const id = setTimeout(function log(){
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
