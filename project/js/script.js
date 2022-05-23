c50aee1463f3d08a0d01d2a73c1eae245817664c
Oleh <oleg_bob@ukr.net>
2022-05-23 14:31:53 +0300

    async/defer

--- project/js/script.js
+++ project/js/script.js
@@ -1,46 +1,94 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
+/*
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+5) Фильмы должны быть отсортированы по алфавиту */
 
 "use strict";
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
 
-const ad = document.querySelectorAll(".promo__adv img");
-const promoGenre = document.querySelector(".promo__genre");
-const poster = document.querySelector(".promo__bg");
-const movies = document.querySelectorAll(".promo__interactive-item");
-const movieList = document.querySelector(".promo__interactive-list");
+  const ad = document.querySelectorAll(".promo__adv img");
+  const promoGenre = document.querySelector(".promo__genre");
+  const poster = document.querySelector(".promo__bg");
+  const movieList = document.querySelector(".promo__interactive-list");
+  const form = document.querySelector("form.add");
+  const addInput = form.querySelector(".adding__input");
+  const checkbox = form.querySelector("[type='checkbox']");
 
-console.log(movies);
-ad.forEach((el) => el.remove());
-promoGenre.innerHTML = "драма";
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newMovie = addInput.value;
+    let favorite = checkbox.checked;
 
-movieList.innerHTML = "";
+    if (newMovie) {
+      if (newMovie.length > 21) {
+        newMovie = `${newFilm.substring(0, 22)}...`;
+      }
 
-movieDB.movies.sort().forEach((el, i) => {
-  movieList.innerHTML += `
-  <li class="promo__interactive-item">
-                ${i + 1} ${el}
-                <div class="delete"></div>
-  `;
+      if (favorite) {
+        console.log("Добавляем любимый фильм");
+      }
+
+      movieDB.movies.push(newMovie);
+      sortArr(movieDB.movies);
+
+      createmovielist(movieDB.movies, movieList);
+    }
+
+    e.target.reset();
+  });
+
+  const deleteAdv = (arr) => {
+    arr.forEach((el) => el.remove());
+  };
+
+  const makeChanges = () => {
+    promoGenre.innerHTML = "драма";
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+  };
+
+  const sortArr = (arr) => {
+    arr.sort();
+  };
+
+  function createmovielist(films, parent) {
+    parent.innerHTML = "";
+    sortArr(movieDB.movies);
+
+    films.forEach((el, i) => {
+      parent.innerHTML += `
+    <li class="promo__interactive-item">
+                  ${i + 1} ${el}
+                  <div class="delete"></div>
+    `;
+    });
+
+    document.querySelectorAll(".delete").forEach((item, i) => {
+      item.addEventListener("click", () => {
+        item.parentElement.remove();
+        movieDB.movies.splice(i, 1);
+        createmovielist(films, parent);
+      });
+    });
+  }
+  deleteAdv(ad);
+  makeChanges();
+
+  createmovielist(movieDB.movies, movieList);
 });
