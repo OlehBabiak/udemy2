1b73b3e634fafa2c63aeffd1cf45703ca71cb865
Oleh <oleg_bob@ukr.net>
2022-05-27 16:07:50 +0300

    some shanges

--- ajax&server/script.js
+++
@@ -1,7 +0,0 @@
-const inputUah = document.querySelector("#UAH"),
-  inputUsd = document.querySelector("#USD");
-
-inputUah.addEventListener("input", () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "");
-});
