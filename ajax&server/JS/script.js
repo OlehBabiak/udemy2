b9e7b71e2fc35492674cdf3a1c8e88a4a166b719
Oleh <oleg_bob@ukr.net>
2022-05-27 16:48:47 +0300

    task done

--- ajax&server/JS/script.js
+++ ajax&server/JS/script.js
@@ -1,7 +1,37 @@
+"use strict";
+
 const inputUah = document.querySelector("#UAH"),
   inputUsd = document.querySelector("#USD");
 
 inputUah.addEventListener("input", () => {
   const request = new XMLHttpRequest();
   request.open("GET", "js/current.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8"); // Самий перший спосіб для роботи з асинхронним кодом
+  request.send();
+
+  // request.addEventListener("readystatechange", () => {
+  //   if (request.readyState === 4 && request.status === 200) {
+  //     console.log(request.response);
+  //     const data = JSON.parse(request.response);
+  //     inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+  //   } else {
+  //     inputUsd.value = "Шось блять не так!";
+  //   }
+  // });
+
+  request.addEventListener("load", () => {
+    //подія виконується коли відповідь повертається
+    if (request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+    } else {
+      inputUsd.value = "Шось блять не так!";
+    }
+  });
+
+  //status
+  // statusText
+  // response
+  // readyState
 });
