7945374f1bbc0a07a1791c5071ce62ded34ade16
Oleh <oleg_bob@ukr.net>
2022-05-30 16:57:28 +0300

    Fetch

--- Promises/script.js
+++ Promises/script.js
@@ -2,47 +2,47 @@
 
 // console.log("Data request");
 
-// const req = new Promise((res, rej) => {
-//   setTimeout(() => {
-//     console.log("Preparing data...");
+const req = new Promise((res, rej) => {
+  setTimeout(() => {
+    console.log("Preparing data...");
 
-//     const prod = {
-//       name: "TV",
-//       price: 2000,
-//     };
+    const prod = {
+      name: "TV",
+      price: 2000,
+    };
 
-//     res(prod);
-//   }, 2000);
-// });
+    res(prod);
+  }, 2000);
+});
 
-// req
-//   .then((prod) => {
-//     return new Promise((res, rej) => {
-//       setTimeout(() => {
-//         prod.status = "ordered";
-//         res(prod);
-//       }, 2000);
-//     });
-//   })
-//   .then((data) => {
-//     data.modify = true;
-//     return data;
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch(() => {
-//     console.error("Error");
-//   })
-//   .finally(() => {
-//     console.log("Every think is OK!");
-//   });
-
-const test = (time) => {
-  return new Promise((res, rej) => {
-    setTimeout(() => res(), time);
+req
+  .then((prod) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        prod.status = "ordered";
+        res(prod);
+      }, 2000);
+    });
+  })
+  .then((data) => {
+    data.modify = true;
+    return data;
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(() => {
+    console.error("Error");
+  })
+  .finally(() => {
+    console.log("Every think is OK!");
   });
-};
+
+// const test = (time) => {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => res(), time);
+//   });
+// };
 
 // test(1000).then(() => {
 //   console.log("1000 ms");
@@ -57,7 +57,17 @@ const test = (time) => {
 //   console.log("All");
 // });
 
-Promise.race([test(1000), test(2000)]).then(() => {
-  //Promise чекає виконання першого промісу з переданих в масив
-  console.log("All");
-});
+// Promise.race([test(1000), test(2000)]).then(() => {
+//   //Promise чекає виконання першого промісу з переданих в масив
+//   console.log("All");
+// });
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({ name: "Alex" }),
+  headers: {
+    "Content-type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
