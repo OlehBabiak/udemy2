623c239561605c7a19b9b604eb979978c9ec1600
Oleh <oleg_bob@ukr.net>
2022-05-30 17:24:27 +0300

    Use fetch insted XMLHttpRequest

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -239,13 +239,13 @@ window.addEventListener("DOMContentLoaded", function () {
       // form.appendChild(statusMessage);
       form.insertAdjacentElement("afterend", statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
+      // const request = new XMLHttpRequest();
+      // request.open("POST", "server.php");
 
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      );
+      // request.setRequestHeader(
+      //   "Content-type",
+      //   "application/json; charset=utf-8"
+      // );
       const formData = new FormData(form);
 
       const object = {}; // formData перетворюєм в JSON
@@ -253,20 +253,39 @@ window.addEventListener("DOMContentLoaded", function () {
       formData.forEach(function (value, key) {
         object[key] = value;
       });
-      const json = JSON.stringify(object);
 
-      request.send(json);
+      // request.send(json);
 
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-          console.log(request.response);
+      fetch("server1.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(object),
+      })
+        .then((data) => data.text())
+        .then((data) => {
+          console.log(data);
           showThanksModal(message.success);
-          form.reset();
           statusMessage.remove();
-        } else {
+        })
+        .catch(() => {
           showThanksModal(message.failure);
-        }
-      });
+        })
+        .finally(() => {
+          form.reset();
+        });
+
+      // request.addEventListener("load", () => {
+      //   if (request.status === 200) {
+      //     console.log(request.response);
+      //     showThanksModal(message.success);
+      //     form.reset();
+      //     statusMessage.remove();
+      //   } else {
+      //     showThanksModal(message.failure);
+      //   }
+      // });
     });
   }
 
